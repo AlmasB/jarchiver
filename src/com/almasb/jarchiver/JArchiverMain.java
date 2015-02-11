82b0e105329fe59880ce832db9948ea9f2eaa8a0
AlmasB <almaslvl@gmail.com>
2015-02-11 22:49:55 +0000

    updated version of mylib, FX mvc

--- src/com/almasb/jarchiver/JArchiverMain.java
+++ src/com/almasb/jarchiver/JArchiverMain.java
@@ -20,29 +20,17 @@
  */
 package com.almasb.jarchiver;
 
-import javafx.scene.Parent;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
+import javafx.application.Application;
 
-public class Popup extends Parent {
-
-    private Text text = new Text();
-
-    public Popup() {
-        Rectangle background = new Rectangle(150, 50);
-        background.setFill(Color.AQUA);
-        background.setStroke(Color.BLUEVIOLET);
-        background.setArcHeight(30);
-        background.setArcWidth(30);
-
-        StackPane stack = new StackPane();
-        stack.getChildren().addAll(background, text);
-        getChildren().add(stack);
-    }
-
-    public void setMessage(String message) {
-        text.setText(message);
+/**
+ * Main entry point
+ *
+ * @author Almas Baimagambetov (ab607@uni.brighton.ac.uk)
+ * @version 1.0
+ *
+ */
+public final class JArchiverMain {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
     }
 }
